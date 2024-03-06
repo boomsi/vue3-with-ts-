6cec70cf6b6353a51df33d2b14a6a0737b6ec157
libo <libo@huanqiu.com>
2024-03-06 19:09:13 +0800

    init

---
+++ src/components/One.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, onMounted, nextTick } from 'vue'
+
+const count = ref(0)
+const id = 'abc'
+const abdDef = 'abcdef'
+const html = '<span>111</span>'
+const obj = {
+    'data-name': 'aaa',
+    'data-age': 18
+}
+
+
+async function increment() {
+    count.value ++
+    await nextTick()
+}
+
+onMounted(() => {
+    console.log('Component is mounted')
+})
+
+</script>
+
+<template>
+    <h3>One</h3>
+    <button @click.prevent="increment">Count is {{ count }}</button>
+    <div v-html="html" :[abdDef]="id" v-bind="obj" :id="id"></div>
+</template>
+
+<style scoped>
+button {
+    font-weight: bold;
+}
+</style>
---
+++ src/components/Two.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { ref } from 'vue';
+
+
+export default {
+    setup() {
+        const count = ref(0)
+
+        function increment() {
+            count.value ++
+        }
+
+        return {count, increment}
+    }
+}
+
+</script>
+
+<template>
+    <h3>Two</h3>
+    <button @click="increment">{{ count }}</button>
+</template>
